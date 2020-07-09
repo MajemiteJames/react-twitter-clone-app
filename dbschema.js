77242a215e7effa688d8d8ed6637ebb2c43efff9
MajemiteJames <majemiteokoro@gmail.com>
2020-07-09 07:55:26 +0100

    created a database model file

---
+++ dbschema.js
@@ -0,0 +1,12 @@
+let db = {
+
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is a scream body',
+            createdAt: "2020-07-08T18:26:38.044Z",
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
