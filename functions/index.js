11c700207b2824b4b02adae8a2b951faadc93605
MajemiteJames <majemiteokoro@gmail.com>
2020-07-09 10:06:17 +0100

    implemented the user signup route which return a token if all credentials are valid pending validation

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,29 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+    apiKey: "AIzaSyCfVOks0FFEKqzvISYWQjN4QsnwCeRCVM4",
+    authDomain: "social-app-1b93f.firebaseapp.com",
+    databaseURL: "https://social-app-1b93f.firebaseio.com",
+    projectId: "social-app-1b93f",
+    storageBucket: "social-app-1b93f.appspot.com",
+    messagingSenderId: "1097110146606",
+    appId: "1:1097110146606:web:beb72e8d286c0d99e799bf",
+    measurementId: "G-PXRVSRL33X"
+  };
+
+
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 app.get('/screams', (request, response) => {
-    admin.firestore().collection('screams').orderBy('createdAt', 'desc').get()
+    db.collection('screams').orderBy('createdAt', 'desc').get()
     .then(data => {
         let screams = [];
         data.forEach(doc => {
@@ -32,13 +48,58 @@ app.post('/scream',(request, response) => {
         createdAt: new Date().toISOString()
     };
 
-    admin.firestore().collection('screams').add(newScream).then( doc => {
+    db.collection('screams').add(newScream).then( doc => {
        return response.json({ message: `document ${doc.id} created successfully`})
     })
     .catch( err => {
         response.status(500).json({ error: 'something went wrong'})
         console.error(err);
     })
+})
+
+// signup route
+app.post('/signup', (request, response) => {
+    const newUser = {
+        email: request.body.email,
+        password: request.body.password,
+        confirmPassword: request.body.confirmPassword,
+        handle: request.body.handle,
+    }
+
+    // validate data
+
+    let newToken, userId;
+    db.doc(`/users/${newUser.handle}`).get().then(doc => {
+        if(doc.exists){
+            return response.status(400).json({ handle: `this handle is already taken`})
+        }else {
+           return  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        }
+    }).then(data => {
+        userId = data.user.uid;
+        return data.user.getIdToken();
+    }).then(token => {
+        newToken = token;
+        const userCredentials = {
+            handle: newUser.handle,
+            email: newUser.email,
+            createdAt: new Date().toISOString(),
+            userId
+        };
+
+        return db.doc(`/users/${newUser.handle}`).set(userCredentials)
+    }).then( () => {
+        return response.status(201).json({ newToken })
+    }).catch( err => {
+        console.error(err);
+        if(err.code === "auth/email-already-in-use"){
+            response.status(400).json({ email: 'Email is already in use'})
+        }else{
+
+            response.status(500).json({ error: err.code})
+        }
+    })
+
 })
 
 
